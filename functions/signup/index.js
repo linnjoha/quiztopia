2fe3378d66d6570bf96759eb4c4acc6e8abdc2cc
linnjoha <linn.wallin@hotmail.com>
2024-10-01 12:06:18 +0200

    username validation

--- functions/signup/index.js
+++ functions/signup/index.js
@@ -1,4 +1,4 @@
-const { docClient, PutCommand } = require("../../service/db");
+const { docClient, PutCommand, GetCommand } = require("../../service/db");
 const middy = require("@middy/core");
 const { v4: uuidv4 } = require("uuid");
 const { hashPassword } = require("../../utils/bcrypt");
@@ -6,7 +6,7 @@ const { sendResponse, sendError } = require("../../responses");
 
 const addUser = async (user) => {
   try {
-    const params = new PutCommand({
+    const command = new PutCommand({
       TableName: "quiztopiaUsersTable",
       Item: {
         userId: user.userId,
@@ -15,11 +15,26 @@ const addUser = async (user) => {
       },
     });
 
-    const { Item } = await docClient.send(params);
+    const { Item } = await docClient.send(command);
     return { data: Item, success: true };
   } catch (error) {
     console.log(error);
     return { success: false };
+  }
+};
+const isThereAUserByUserName = async (userName) => {
+  try {
+    const command = new GetCommand({
+      TableName: "quiztopiaUsersTable",
+      Key: { userName: userName },
+    });
+    const { Item } = await docClient.send(command);
+    if (Item) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -38,6 +53,10 @@ const handler = middy().handler(async (event) => {
       userName: userName,
       password: hashedPassword,
     };
+    const isUserNameAvailable = await isThereAUserByUserName(userName);
+    if (!isUserNameAvailable) {
+      return sendError(400, "userName not available");
+    }
     const response = await addUser(newUser);
     if (!response.success) {
       return sendError(500, "Could not add user to db");
